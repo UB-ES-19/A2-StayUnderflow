e8da96a16dce8b27291dc8ec59424dc2ad5c6c1b
ArnauNaval <arnaunaval@telefonica.net>
2019-09-25 18:35:41 +0200

    Hotfix:)

--- StayUnderflow_WebApp/urls.py
+++ StayUnderflow_WebApp/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    #url(r'', views.stayunderflow, name='stayunderflow'),
+    url(r'^$', views.stayunderflow, name='stayunderflow'),
     url(r'^stayunderflow/$', views.stayunderflow, name='stayunderflow'),
     url(r'^register/$', views.register, name='register'),
     url(r'^login/$', views.login_m, name='login'),
