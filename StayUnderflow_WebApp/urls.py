d1ed0c5c38e56947d0129b41b63e69c5fda3cc5d
CarlaMontserrat <carlamontserrat16@gmail.com>
2019-10-08 17:38:05 +0200

    Coses varies de links arreglades

--- StayUnderflow_WebApp/urls.py
+++ StayUnderflow_WebApp/urls.py
@@ -13,7 +13,6 @@ urlpatterns = [
     url(r'^auth/', include('social_django.urls', namespace='social')),
     url(r'^profile/$', views.my_profile, name='profile'),
     url(r'^users/$', views.search_users, name='users'),
-    url(r'^users/(?P<username>.*)/$', views.search_users, name='users'),
     path('stayunderflow/post/<int:pk>/', PostDetailView.as_view(), name='post-detail'), # pk serà un enter que contindrà el id del post clicat
     path('stayunderflow/post/new/', CreatePost.as_view(), name='new-post'),
     path('stayunderflow/post/<int:pk>/answer/', CreateAnswer.as_view(), name='new-answer'),
