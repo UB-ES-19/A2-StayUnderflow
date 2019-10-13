4be2c79009e609b06583f2b0dc02cc5aa95500a2
Joan Trave <jtravegordillo@gmail.com>
2019-10-13 12:35:32 +0200

    tags in post model

--- StayUnderflow_WebApp/models.py
+++ StayUnderflow_WebApp/models.py
@@ -1,16 +1,10 @@
 from django.db import models
 from django import forms
 from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
+
 from utils.time import get_time_local
 from django.urls import reverse
-
-class Tag(models.Model):
-
-    # Nom del tag
-    name = models.TextField()
-
-    def __str__(self):
-        return self.name
 
 # Classe que guardarà la informació de cada post
 class Post(models.Model):
@@ -20,7 +14,7 @@ class Post(models.Model):
     date_posted = models.DateTimeField(default=get_time_local()) # en el cas de no posar data es posa la data actual del sistema per defecte
     author = models.ForeignKey(User, on_delete= models.CASCADE) # quan s'elimina un usuari també s'eliminen els seus posts
 
-    tags = models.ManyToManyField(Tag)
+    tags = TaggableManager()
 
     def __str__(self):
         return self.title
